8cbc2823c5423a73fde76e7d0a0d945e01709f70
Juan Solano <bautista.solano@gmail.com>
2023-07-18 19:14:47 -0400

    Updating files

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/JuanSolano/LATAM-Tech-Box.git
+##git clone https://github.com/JuanSolano/LATAM-Tech-Box.git
 mv LATAM-Tech-Box/videos-touch-1/* videos
 rm -rf LATAM-Tech-Box
 ##mv * ~/
