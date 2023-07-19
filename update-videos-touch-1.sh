fbcde3de8ac12f11ca8f411410bfa0904091b185
Juan Solano <bautista.solano@gmail.com>
2023-07-19 19:06:35 -0400

    Updating files

--- update-videos-touch-1.sh
+++ update-videos-touch-1.sh
@@ -4,4 +4,4 @@ git clone https://github.com/JuanSolano/LATAM-Tech-Box.git
 sudo cp LATAM-Tech-Box/videos-touch-1/* http_server/videos
 rm -rf LATAM-Tech-Box
 echo "Videos updated. Will reboot in 5 secs"
-sleep 5
+sudo reboot
--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@ sudo cp LATAM-Tech-Box/videos-touch-1/* http_server/videos
 rm -rf LATAM-Tech-Box
 echo "Videos updated. Will reboot in 5 secs"
 sleep 5
+sudo reboot
